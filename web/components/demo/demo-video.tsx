ac68690c0eed018d37bc0e56b5827494aaada608
Jimmy <jimmychavada22@gmail.com>
2026-06-15 00:07:18 -0400

    Cache-bust demo video

--- web/components/demo/demo-video.tsx
+++ web/components/demo/demo-video.tsx
@@ -39,7 +39,7 @@ export function DemoVideo() {
     >
       <video
         ref={ref}
-        src="/cachelane.mp4"
+        src="/cachelane.mp4?v=2"
         muted
         playsInline
         loop
